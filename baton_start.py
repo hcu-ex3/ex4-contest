2efada2ed2ce3a75de4916904aae7bd531218515
ikepe <main@ikepe.xyz>
2023-01-11 16:02:42 +0900

    スタートの処理

---
+++ baton_start.py
@@ -0,0 +1,32 @@
+# 開始時に実行
+# 緑を検知し、距離センサも反応したら回転しスタートする
+
+from rotation import rotation
+
+import cv2
+import r3pi
+import time
+import numpy as np
+import pass_baton as pb
+
+def get_distance(criterion):
+  distance = r3pi.pot_voltage()
+  if distance < criterion:
+    return True
+  else:
+    return False
+
+def rotation(wait_time):
+  r3pi.stop
+  r3pi.left_motor(0.25)
+  r3pi.right_motor(-0.25)
+  time.sleep(wait_time)
+  r3pi.stop
+        
+def check_start(cap):
+  _, frame0 = cap.read()
+  while not get_distance(550) or not pb.isPassBaton(frame0):
+    _, frame0 = cap.read()
+    time.sleep(0.1)
+  rotation(0.55)
+
